352aed07412fab13cead633bc6c9aa0a9d4e972c
Francesco Romano <francesco.romano.1987@gmail.com>
2017-06-16 10:13:09 +0200

    Added YCM and Robot-Testing formulae

---
+++ robot-testing.rb
@@ -0,0 +1,36 @@
+class RobotTesting < Formula
+  desc "Robot Testing Framework (RTF)"
+  homepage "http://robotology.github.io/robot-testing/index.html"
+
+  stable do
+    url "https://github.com/robotology/robot-testing/archive/v1.2.0.tar.gz"
+    sha256 "d5147e190647b576c0219759467dfc7663a8262672cb8ef5209a7c455488b2e4"
+  end
+
+  head do
+    url "https://github.com/robotology/robot-testing.git", :branch => "master"
+  end
+
+
+  depends_on "cmake" => :build
+
+  depends_on "python" => :optional
+  depends_on "lua" => :optional
+  depends_on "ruby" => :optional
+
+  def install
+    args = std_cmake_args
+
+
+    args << "-DENABLE_LUA_PLUGIN:BOOL=ON" if build.with? "lua"
+    args << "-DENABLE_PYTHON_PLUGIN:BOOL=ON" if build.with? "python"
+    args << "-DENABLE_RUBY_PLUGIN:BOOL=ON" if build.with? "ruby"
+
+    system "cmake", *args
+    system "make", "install"
+  end
+
+  test do
+
+  end
+end
