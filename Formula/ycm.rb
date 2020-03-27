4f2516d323df75b7a4dc4f4cbfeee1b34d6f447c
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2020-03-27 15:09:02 +0100

    Update YCM formula to 0.11.1
    Also start using the offline package

--- Formula/ycm.rb
+++ Formula/ycm.rb
@@ -3,8 +3,8 @@ class Ycm < Formula
   homepage "https://robotology.github.io/ycm"
 
   stable do
-    url "https://github.com/robotology/ycm/archive/v0.11.0.tar.gz"
-    sha256 "b489a7d0f452fdc92ad3af83385896cac9f6f6222e7b3b920412f81b787f4376"
+    url "https://github.com/robotology/ycm/releases/download/v0.11.1/ycm-0.11.1-offline.tar.gz"
+    sha256 "f7f3c2641daaade3237ff6c0a1a6c03a2e231841a86b9739acb71dd0de1a324b"
   end
 
   head do
