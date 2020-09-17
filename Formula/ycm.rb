bd22fecfa22786853ef1e1e775ba7f56f59065a2
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2020-09-17 12:39:28 +0200

    Update ycm formula to 0.11.4

--- Formula/ycm.rb
+++ Formula/ycm.rb
@@ -3,8 +3,8 @@ class Ycm < Formula
   homepage "https://robotology.github.io/ycm"
 
   stable do
-    url "https://github.com/robotology/ycm/releases/download/v0.11.3/ycm-0.11.3-offline.tar.gz"
-    sha256 "5fd381298200f209befddc8c6ab9857a5716ce1b172cbb876281fc2dcd4977a6"
+    url "https://github.com/robotology/ycm/releases/download/v0.11.4/ycm-0.11.4-offline.tar.gz"
+    sha256 "5029c6ef3d2d9697e69ff0450951aa65dfda4fc7ff73618c345d1a4bac62b584"
   end
 
   head do
